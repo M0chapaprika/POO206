b77a31555aa6fd43b9ba7d7c3420faeb83299e1c
M0chapaprika <yanyan2.0xd@gmail.com>
2025-05-16 18:19:07 -0600

    Variab;es

--- Practica1/sintaxis.py
+++ Practica1/sintaxis.py
@@ -7,6 +7,27 @@ de varias lineas
 en python
 """ 
 
-# 2.- Strings
-print("hols soy una cadena")
+#2.- Strings
 print('yo soy otra')
+variable1= "hola soy una cadena"
+print(len(variable1)) #len sirve para contrar los caracteres
+print(variable1[2:5])
+print(variable1[2:])
+print(variable1[:5])
+
+#3.- Variables
+
+x="ivan isaay"
+x=4
+x=5.76
+print(x)
+
+x= int(3)
+y= float(3)
+z=str(3) 
+
+print(x,y,z)
+print(type(x))
+print(type(y))
+print(type(z))
+
