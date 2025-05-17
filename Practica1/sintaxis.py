4e5bb2ea36ac6d5b50fbc99863a3097e7adb341f
M0chapaprika <yanyan2.0xd@gmail.com>
2025-05-16 18:36:26 -0600

    solicitud y logistica de datos

--- Practica1/sintaxis.py
+++ Practica1/sintaxis.py
@@ -8,26 +8,48 @@ en python
 """ 
 
 #2.- Strings
-print('yo soy otra')
-variable1= "hola soy una cadena"
-print(len(variable1)) #len sirve para contrar los caracteres
-print(variable1[2:5])
-print(variable1[2:])
-print(variable1[:5])
+#print('yo soy otra')
+#variable1= "hola soy una cadena"
+#print(len(variable1)) #len sirve para contrar los caracteres
+#print(variable1[2:5])
+#print(variable1[2:])
+#print(variable1[:5])
 
 #3.- Variables
 
-x="ivan isaay"
-x=4
-x=5.76
-print(x)
+#x="ivan isaay"
+#x=4
+#x=5.76
+#print(x)
 
-x= int(3)
-y= float(3)
-z=str(3) 
+#x= int(3)
+#y= float(3)
+#z=str(3) 
 
-print(x,y,z)
-print(type(x))
-print(type(y))
-print(type(z))
+#print(x,y,z)
+#print(type(x))
+#print(type(y))
+#print(type(z))
 
+#4.- solicitud de datos
+
+#a= input("introduce cualquier dato: ")
+
+#b= int(input("introduce un numero entero: "))
+#c= float(input("introduce un numero decimal: "))
+
+#print(a,b,c)
+
+#5.- boolean, comparacion y logicos
+
+print(10>9)
+print(10<9)
+print(10==9)
+print(10>=9)
+print(10<=9)
+print(10!=9)
+
+x= 1
+print(x<5 and x<10)
+print(x<5 or x<10)
+print(not(x<5 and x<10))
