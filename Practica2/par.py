9d2e589f9e156316408fb73f919c67164f24f51c
M0chapaprika <yanyan2.0xd@gmail.com>
2025-05-22 19:47:29 -0600

    Par-impar

---
+++ Practica2/par.py
@@ -0,0 +1,13 @@
+
+for i in range (0,5):
+    try:
+        num1=int(input("Ingresa un numero: "))
+    
+        if num1 % 2 == 0:
+            print("El numero es par: ", num1)
+        else: 
+            print("El numero es impar: ", num1)
+        break
+
+    except ValueError:
+        print("Error: Se ingreso algo que no es un numero entero.")   
