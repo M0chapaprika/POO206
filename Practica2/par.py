60f8b8d51c23d1629686c7578e12d8f05032b4a5
M0chapaprika <yanyan2.0xd@gmail.com>
2025-05-22 20:16:41 -0600

    par-impar full
    ahora si no acepta los negativos :)

--- Practica2/par.py
+++ Practica2/par.py
@@ -3,12 +3,14 @@ for i in range (0,5):
     try:
         num1=int(input("Ingresa un numero: "))
     
-        if num1 % 2 == 0:
+        if num1 % 2 == 0 and num1 > 0:
             print("El numero es par: ", num1)
-        else: 
+        elif num1 % 2 == 1 and num1 > 0: 
             print("El numero es impar: ", num1)
+        else:
+            print("El numero no puede ser negativo >:(")
         break
-
+    
     except ValueError:
         print("Error: Se ingreso algo que no es un numero entero.")   
         
