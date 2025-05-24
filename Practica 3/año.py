f23151afcbc956ebe617768f12ee088ed8c81c13
M0chapaprika <yanyan2.0xd@gmail.com>
2025-05-23 19:25:09 -0600

    año
    Este codigo calcula si el año ingresado es bisiesto o no, impidiendo que se ingresen numeros negativos

---
+++ Practica 3/año.py
@@ -0,0 +1,15 @@
+for i in range(0, 5):
+    try:
+        año = int(input("Ingresa un año: "))
+        
+        if año > 0:
+            if (año % 4 == 0 and año % 100 != 0) or (año % 400 == 0):
+                print("El año es bisiesto:", año)
+            else:
+                print("El año no es bisiesto:", año)
+        else:
+            print("No se aceptan valores negativos >:(")
+        break
+    
+    except ValueError:
+        print("Error: Se ingresó algo que no es un número entero.")
