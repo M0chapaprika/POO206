ffb12cd6004c91b4e50e65e4f8d758b62117af86
M0chapaprika <yanyan2.0xd@gmail.com>
2025-06-05 20:01:33 -0600

    par-impar
    En este codigo se solicta un numero y dependiendo de si es par o impar se hara un proceso diferente, si es par se divide entre 2 y si es impar se multiplica por 3 y se le suma 1, el ciclo se detiene hasta llegar a 1

---
+++ Examen/Collatz.py
@@ -0,0 +1,18 @@
+
+for i in range(1,5):
+    
+    try:
+        numero1=(int(input("Ingresa un numero mayor a 1: ")))
+        
+        if numero1 >= 1:
+            for num in range(numero1, -10, -1):
+                if numero1 % 2 == 0:
+                    numero1 = numero1 / 2
+                    print(numero1)
+                else:
+                    numero1 = (numero1 * 3) + 1
+                    print(numero1)
+        else:
+            print("Debes ingresar un numero mayor a 1")
+    except ValueError:
+        print("Error: ", ValueError)
